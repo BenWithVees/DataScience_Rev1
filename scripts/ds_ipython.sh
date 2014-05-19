1be66dca72bd16aeb42177cfaf30691ab06acbf6
root <root@ubuntu.(none)>
2014-05-19 15:33:55 -0600

    Fixed docker image name

--- scripts/ds_ipython.sh
+++ scripts/ds_ipython.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 echo "Starting IPython Notebook Node..."
-CID_ipython=$(docker run -d --privileged --dns 127.0.0.1 -p 8888:8888 --name ipython -h ipython -i -t hwx/ipython)
+CID_ipython=$(docker run -d --privileged --dns 127.0.0.1 -p 8888:8888 --name ipython -h ipython -i -t hwx/ipython_node)
 IP_ipython=$(docker inspect --format "{{ .NetworkSettings.IPAddress }}" ipython)
 echo "IPython Notebook Server Started at $IP_ipython"
