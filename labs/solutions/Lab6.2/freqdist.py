4689f745e459dafa1c458bff63d6da75d04e62d8
rfraposa <rich@hortonworks.com>
2014-05-23 16:52:38 -0600

    Added punctuation

--- labs/solutions/Lab6.2/freqdist.py
+++ labs/solutions/Lab6.2/freqdist.py
@@ -1,15 +1,29 @@
+
+# coding: utf-8
+
+# In[ ]:
+
 #!/usr/bin/python
 
 import fileinput
+import string
 from nltk import FreqDist
 from nltk.tokenize import RegexpTokenizer
 
-lines = ""
+replace_punctuation = string.maketrans(string.punctuation, ' '*len(string.punctuation))
+cleaned_input = ""
 for line in fileinput.input():
-	line=line.strip()
-	lines += line + " "
+    line=line.strip()
+    line=line.translate(replace_punctuation)
+    cleaned_input += line + " "
 
 tokenizer = RegexpTokenizer(r'\w+')
-freq = FreqDist(tokenizer.tokenize(lines))
+freq = FreqDist(tokenizer.tokenize(cleaned_input))
 for k in freq.keys()[:50]:
-	print "%s\t%s" % (k, freq[k])
+    print "%s\t%s" % (k, freq[k]) 
+
+
+# In[ ]:
+
+
+
